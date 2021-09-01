abfc7f6f449680bad44cfda1e62587824afe6aed
samplesn08 <samplesn08@gmail.com>
2021-09-01 09:46:48 -0400

    clean up ReadMe, test file, and added comments in CSS file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the app as expected', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/your wall/i);
+  expect(element).toBeInTheDocument();
 });
